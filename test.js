66ee0d99e49672a50f4a1d882156556c5ae7749c
Bod <keissy.bod@valdemarne.fr>
2023-01-31 14:44:58 +0100

    reforme and add minute and hour

--- test.js
+++ test.js
@@ -1,13 +1,45 @@
-function testFunction() {
-    // get the div element from the popup and set its innerHTML
-    var testDiv = document.getElementById("testDiv");
-    testDiv.innerHTML = "Hello World!";
+function getMenu() {
+
+    let time = "error";
+
+    switch (true){
+        case document.getElementById("radio-7h").checked :
+            time = "7H";
+            break;
+        case document.getElementById("radio-7h30").checked :
+            time = "7H30";
+            break;
+        case document.getElementById("radio-7h48").checked :
+            time = "7H48";
+            break;
+        case document.getElementById("radio-8h20").checked :
+            time = "8H20";
+            break;
+        case document.getElementById("radio-8h40").checked :
+            time = "8H40";
+            break;
+    }
+
+    return time
 }
-document.getElementById("DOMBtn").addEventListener("click", testFunction);
+
+function getInputTime(){
+    const time = document.getElementById("input-arrive").value
+
+    console.log(time)
+    return time
+}
+
+function engineController(){
+    const testDiv = document.getElementById("inputGo");
 
 
-function OpenURL(){
-    chrome.tabs.create({ url: "https://nicolas.brondin-bernard.com" });
+    const menu = getMenu();
+    const time= getInputTime();
+
+    testDiv.value = time;
+
+
 }
-document.getElementById("openBtn").addEventListener("click", OpenURL);
+document.getElementById("BtnRun").addEventListener("click", engineController);
 
